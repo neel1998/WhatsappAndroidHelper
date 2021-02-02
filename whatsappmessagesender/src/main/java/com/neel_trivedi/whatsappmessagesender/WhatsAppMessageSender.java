a99c3bac37e886c3d11d68ecf3dc0f9751028a5f
neel1998 <neellovepainting@gmail.com>
2021-02-02 12:09:24 +0530

    java doc added

--- whatsappmessagesender/src/main/java/com/neel_trivedi/whatsappmessagesender/WhatsAppMessageSender.java
+++ whatsappmessagesender/src/main/java/com/neel_trivedi/whatsappmessagesender/WhatsAppMessageSender.java
@@ -10,10 +10,23 @@ import java.net.URLEncoder;
 public class WhatsAppMessageSender {
 
     private Context mContext;
+
+    /**
+     * Class constructor
+     * @param context Context of the calling class/activity
+     */
     public WhatsAppMessageSender(Context context) {
         mContext = context;
     }
 
+
+    /**
+     * Method to send message from app to whatsapp
+     * @param countryCode phone number country code without preceding "+"
+     * @param contactNumber valid phone number string
+     * @param message message to added to the whatsapp text box
+     * @throws UnsupportedEncodingException
+     */
     public void sendMessage(String countryCode, String contactNumber, String message) throws UnsupportedEncodingException {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         String data = "https://api.whatsapp.com/send?phone=" + countryCode + contactNumber + "&text=" + URLEncoder.encode(message, "UTF-8");
